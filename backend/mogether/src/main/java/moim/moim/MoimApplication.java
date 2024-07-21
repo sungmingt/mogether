4b42b2bc4683a4e05b3789c86573756cd3b31f75
sungmingt <qotjdals8313@naver.com>
2024-07-21 16:03:31 +0900

    config : change project name

--- backend/mogether/src/main/java/moim/moim/MoimApplication.java
+++ backend/mogether/src/main/java/moim/moim/MoimApplication.java
@@ -2,8 +2,10 @@ package moim.moim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MoimApplication {
 
 	public static void main(String[] args) {
