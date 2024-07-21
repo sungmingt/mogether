ec427b9fededdcf71b0da79e159ed41352097c9b
sungmingt <qotjdals8313@naver.com>
2024-07-21 15:46:09 +0900

    config : dependency, config for swagger

---
+++ backend/moim/src/main/java/moim/moim/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package moim.moim.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String TITLE = "API for mo-gether";
+    private static final String DESCRIPTION = "API Docs for mo-gether";
+    private static final String VERSION = "V1.0.0";
+
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(TITLE)
+                        .description(DESCRIPTION)
+                        .version(VERSION)
+                );
+    }
+}
