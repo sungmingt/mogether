db5764c31b6b07380602b53cbba3e41c925311f4
sungmingt <qotjdals8313@naver.com>
2024-08-08 20:13:16 +0900

    fix error

--- backend/mogether/src/test/java/mogether/mogether/MogetherApplicationTests.java
+++ backend/mogether/src/test/java/mogether/mogether/MogetherApplicationTests.java
@@ -3,7 +3,7 @@ package mogether.mogether;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MogetherApplicationTests.class)
 class MogetherApplicationTests {
 
 	@Test
