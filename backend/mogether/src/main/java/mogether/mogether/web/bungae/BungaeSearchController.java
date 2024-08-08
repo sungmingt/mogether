092d49326df8c16385c3ddec604be5d649b61031
sungmingt <qotjdals8313@naver.com>
2024-08-09 00:12:27 +0900

    fix error

--- backend/mogether/src/main/java/mogether/mogether/web/bungae/BungaeSearchController.java
+++ backend/mogether/src/main/java/mogether/mogether/web/bungae/BungaeSearchController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import mogether.mogether.domain.bungae.Bungae;
+//import mogether.mogether.application.bungae.BungaeService;
 import mogether.mogether.web.bungae.dto.BungaeListResponse;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +17,8 @@ import java.util.List;
 @RequestMapping("/bungae/search")
 public class BungaeSearchController {
 
+//    private final BungaeService bungaeService;
+
     @Operation(summary = "번개 검색", description = "제목, 위치(시, 구) 기반 번개 검색",
             responses = {
                     @ApiResponse(responseCode = "200", description = "번개 검색 성공"),
@@ -24,8 +26,8 @@ public class BungaeSearchController {
     @GetMapping
     public List<BungaeListResponse> search(@RequestParam(required = false) String name,
                                            @RequestParam(required = false) String city,
-                                           @RequestParam(required = false) String gu,
-                                           @AuthenticationPrincipal PrincipalDetail principalDetail) {
-        return bungaeService.searchByAddress(name, city, gu, principalDetail);
+                                           @RequestParam(required = false) String gu) {
+//        return bungaeService.searchByAddress(name, city, gu, principalDetail);
+        return new ArrayList<>();
     }
 }
