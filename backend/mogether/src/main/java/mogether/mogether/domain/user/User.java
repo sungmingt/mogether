d8ff75a57ad36c4022bd2022d9a13205933ea8bb
sungmingt <qotjdals8313@naver.com>
2024-08-09 15:45:55 +0900

    feat: add domain logic

--- backend/mogether/src/main/java/mogether/mogether/domain/user/User.java
+++ backend/mogether/src/main/java/mogether/mogether/domain/user/User.java
@@ -1,19 +1,23 @@
 package mogether.mogether.domain.user;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
-import mogether.mogether.domain.Address;
+import mogether.mogether.domain.info.Address;
 import mogether.mogether.domain.bungae.Bungae;
 import mogether.mogether.domain.bungae.BungaeUser;
-import mogether.mogether.domain.interest.BungaeInterest;
-import mogether.mogether.domain.interest.MoimInterest;
+import mogether.mogether.domain.interest.bungae.BungaeInterest;
+import mogether.mogether.domain.interest.moim.MoimInterest;
 import mogether.mogether.domain.moim.Moim;
+import mogether.mogether.domain.moim.MoimUser;
+import mogether.mogether.domain.user.image.ProfileImage;
+import mogether.mogether.domain.info.Gender;
+import mogether.mogether.domain.info.SocialType;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.CascadeType.REMOVE;
 import static jakarta.persistence.FetchType.LAZY;
 
 @Getter
@@ -26,23 +30,28 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String username = "username"; ///////////////
+
     @OneToOne(fetch = LAZY, cascade = ALL, orphanRemoval = true)
     @JoinColumn(name = "image_id")
     private ProfileImage profileImage;
 
-    @OneToMany(mappedBy = "host", cascade = ALL)
+    @OneToMany(mappedBy = "host", cascade = REMOVE)
     private List<Bungae> bungaeHostList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "host", cascade = ALL)
+    @OneToMany(mappedBy = "host", cascade = REMOVE)
     private List<Moim> moimHostList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user") //todo: orphan, cascade 설정
+    @OneToMany(mappedBy = "user", cascade = REMOVE)
     private List<BungaeUser> bungaeJoinList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = REMOVE)
+    private List<MoimUser> moimJoinList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = REMOVE)
     private List<BungaeInterest> bungaeInterestList = new ArrayList<>();
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = REMOVE)
     private List<MoimInterest> moimInterestList = new ArrayList<>();
 
     private String imageUrl;
@@ -53,8 +62,8 @@ public class User {
     @Enumerated(EnumType.STRING)
     private SocialType socialType;
     private String providerId;
-//    private String provider; //////
-    private String name;
+    private String providerKey; //socialType과 providerId를 결합한 고유 값
+
     private String nickname;
 
     @Embedded
@@ -73,8 +82,7 @@ public class User {
         this.imageUrl = fileUrl;
     }
 
-    public void update(String name, String nickname, Address address, int age, Gender gender, String intro, String phoneNumber) {
-        this.name = name;
+    public void update(String nickname, Address address, int age, Gender gender, String intro, String phoneNumber) {
         this.nickname = nickname;
         this.address = address;
         this.age = age;
@@ -87,15 +95,31 @@ public class User {
         this.password = newPassword;
     }
 
-    public User(String email, String password, String name, String nickname, Address address, int age, Gender gender, String intro, String phoneNumber) {
+    public User(String email, String password, String nickname, Address address, int age, Gender gender, String intro, String phoneNumber) {
         this.email = email;
         this.password = password;
-        this.name = name;
         this.nickname = nickname;
         this.age = age;
         this.intro = intro;
         this.gender = gender;
         this.address = address;
         this.phoneNumber = phoneNumber;
+    }
+
+    //todo: principalDetail 수정 후 삭제 필요
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User(String nickname, String email, String imageUrl) {
+        this.nickname = nickname;
+        this.email = email;
+        this.imageUrl = imageUrl;
+    }
+
+    public User(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
     }
 }
