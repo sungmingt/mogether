70ca75429c18ab67a2fa680eea5925d2fdfa5ec0
sungmingt <qotjdals8313@naver.com>
2024-08-09 18:26:28 +0900

    feat: add error code

--- backend/mogether/src/main/java/mogether/mogether/exception/ErrorCode.java
+++ backend/mogether/src/main/java/mogether/mogether/exception/ErrorCode.java
@@ -17,6 +17,9 @@ public enum ErrorCode {
     PASSWORD_NOT_VALID(400, "비밀번호 형식이 올바르지 않습니다."),
     PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다."),
 
+    //AUTH
+    ILLEGAL_REGISTRATION_ID(400, "잘못된 registration id입니다."),
+
     //TOKEN
     ACCESS_TOKEN_EXPIRED(401, "access token 만료, reissue 요청이 필요합니다."),
     REFRESH_TOKEN_EXPIRED(401, "refresh token 만료, 강제 로그아웃"),
@@ -25,6 +28,8 @@ public enum ErrorCode {
     REQUIRED_TOKEN_MISSING(401, "토큰이 존재하지 않습니다."),
     TOKEN_FROM_BLACKLIST(401, "blacklist에 등록된 토큰입니다."),
     PAYLOAD_NOT_VALID(401, "토큰의 payload가 유효하지 않습니다."),
+    TOKEN_INVALID(401, "유효하지 않은 토큰입니다."),
+    TOKEN_SIGNATURE_INVALID(401, "올바르지 않은 토큰 시그니처입니다."),
 
     //FILE
     IMAGE_NOT_FOUND(404, "이미지가 존재하지 않습니다."),
@@ -38,6 +43,7 @@ public enum ErrorCode {
 
     //MOIM
     MOIM_NOT_FOUND(404, "존재하지 않는 모임입니다."),
+    NOT_MOIM_MEMBER(404, "모임 가입 유저가 아닙니다."),
 
     //INTEREST
     INTEREST_ALREADY_EXISTS(400, "이미 좋아요를 누른 글입니다."),
