18955fc9aabc9609b47c8ab7e60bba42a0629c5b
sungmingt <qotjdals8313@naver.com>
2024-08-11 23:33:16 +0900

    feat: add login/logout auth api and logic

---
+++ backend/mogether/src/main/java/mogether/mogether/application/auth/AuthService.java
@@ -0,0 +1,88 @@
+package mogether.mogether.application.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mogether.mogether.domain.oauth.TokenProvider;
+import mogether.mogether.domain.redis.BlackListTokenRepository;
+import mogether.mogether.domain.redis.RefreshTokenRepository;
+import mogether.mogether.domain.user.User;
+import mogether.mogether.domain.user.UserRepository;
+import mogether.mogether.application.user.UserValidator;
+import mogether.mogether.exception.MogetherException;
+import mogether.mogether.web.auth.dto.LoginRequest;
+import mogether.mogether.web.auth.dto.Token;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static mogether.mogether.exception.ErrorCode.*;
+import static mogether.mogether.exception.ErrorCode.EMAIL_NOT_EXISTS;
+import static mogether.mogether.exception.ErrorCode.PASSWORD_NOT_MATCH;
+
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final BlackListTokenRepository blackListTokenRepository;
+
+    public Token login(LoginRequest loginRequest) {
+        User user = findByEmail(loginRequest.getEmail());
+        checkPassword(loginRequest.getPassword(), user.getPassword());
+
+        Long userId = user.getId();
+        return issueToken(userId);
+    }
+
+    public void logout(String accessToken) {
+        //refresh token 삭제
+        Long userId = tokenProvider.getIdFromToken(accessToken);
+        refreshTokenRepository.deleteById(userId);
+
+        //blacklist 등록
+        Long timeToLiveLeft = tokenProvider.getTimeToLiveLeft(accessToken);
+        blackListTokenRepository.setBlackList(accessToken, timeToLiveLeft);
+    }
+
+    public Token reissueToken(String refreshToken) {
+        Long userId = tokenProvider.getIdFromToken(refreshToken);
+
+        //refresh token 검증
+        tokenProvider.validateToken(refreshToken);
+        validateStoredRefreshToken(refreshToken, userId);
+
+        //reissue token
+        String newAccessToken = tokenProvider.createAccessToken(userId);
+        String newRefreshToken = tokenProvider.createRefreshToken(userId);
+        refreshTokenRepository.save(userId, newRefreshToken);
+        return new Token(newAccessToken, newRefreshToken);
+    }
+
+    public Token issueToken(Long userId) {
+        String accessToken = tokenProvider.createAccessToken(userId);
+        String refreshToken = tokenProvider.createRefreshToken(userId);
+        refreshTokenRepository.save(userId, refreshToken);
+        return new Token(accessToken, refreshToken);
+    }
+
+    private void validateStoredRefreshToken(String refreshToken, Long userId) {
+        String storedRefreshToken = refreshTokenRepository.findById(userId)
+                .orElseThrow(() -> new MogetherException(REFRESH_TOKEN_INVALID));
+        tokenProvider.checkRefreshTokenSameness(refreshToken, storedRefreshToken);
+    }
+
+    private void checkPassword(String input, String exact) {
+        String expected = UserValidator.encodePassword(input);
+        if(!expected.equals(exact)){
+            throw new MogetherException(PASSWORD_NOT_MATCH);
+        }
+    }
+
+    private User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new MogetherException(EMAIL_NOT_EXISTS));
+    }
+}
---
+++ backend/mogether/src/main/java/mogether/mogether/web/auth/AuthController.java
@@ -0,0 +1,43 @@
+package mogether.mogether.web.auth;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import mogether.mogether.application.auth.AuthService;
+import mogether.mogether.web.auth.dto.LoginRequest;
+import mogether.mogether.web.auth.dto.Token;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import static mogether.mogether.domain.redis.TokenInfo.ACCESS_TOKEN;
+import static mogether.mogether.domain.redis.TokenInfo.REFRESH_TOKEN;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public HttpStatus login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        Token token = authService.login(loginRequest);
+        response.setHeader(ACCESS_TOKEN, token.getAccessToken());
+        response.setHeader(REFRESH_TOKEN, token.getRefreshToken());
+        return HttpStatus.OK;
+    }
+
+    @PostMapping("/logout")
+    public HttpStatus logout(@RequestHeader(ACCESS_TOKEN) String accessToken) {
+        authService.logout(accessToken);
+        return HttpStatus.NO_CONTENT;
+    }
+
+    @GetMapping("/token")
+    public HttpStatus reissueToken(@RequestHeader String refreshToken, HttpServletResponse response) {
+        Token token = authService.reissueToken(refreshToken);
+        response.setHeader(ACCESS_TOKEN, token.getAccessToken());
+        response.setHeader(REFRESH_TOKEN, token.getRefreshToken());
+        return HttpStatus.OK;
+    }
+}
