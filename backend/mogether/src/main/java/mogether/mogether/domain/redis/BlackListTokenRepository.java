f3a5e5a916e701d2d6704f77382ccf4943427443
sungmingt <qotjdals8313@naver.com>
2024-08-11 23:33:41 +0900

    feat: add redis token repositories

---
+++ backend/mogether/src/main/java/mogether/mogether/domain/redis/BlackListTokenRepository.java
@@ -0,0 +1,30 @@
+package mogether.mogether.domain.redis;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+@Repository
+public class BlackListTokenRepository {
+
+    private static final String BLACKLIST_PREFIX = "blackList:";
+
+    private final ValueOperations<String, String> valueOperations;
+
+    public BlackListTokenRepository(RedisTemplate<String, String> blackListTokenRedisTemplate) {
+        this.valueOperations = blackListTokenRedisTemplate.opsForValue();
+    }
+
+    public void setBlackList(String token, Long timeToLive) {
+        String key = BLACKLIST_PREFIX + token;
+        valueOperations.set(key, token, timeToLive, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean isFromBlacklist(String accessToken) {
+        String key = BLACKLIST_PREFIX + accessToken;
+        String value = valueOperations.get(key);
+        return value != null && !value.isEmpty();
+    }
+}
