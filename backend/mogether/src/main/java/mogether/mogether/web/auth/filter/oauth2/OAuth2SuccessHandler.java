4ae1e236caf91ce9249ca5583ba9b1033a469943
sungmingt <qotjdals8313@naver.com>
2024-08-14 17:28:29 +0900

    feat: add frontend redirect-uri after oauth2 login

--- backend/mogether/src/main/java/mogether/mogether/web/auth/filter/oauth2/OAuth2SuccessHandler.java
+++ backend/mogether/src/main/java/mogether/mogether/web/auth/filter/oauth2/OAuth2SuccessHandler.java
@@ -25,7 +25,7 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     //사용자 정보를 DB에 저장하고, 서비스 자체 액세스 토큰, 리프레시 토큰을 생성/저장
 
     private final AuthService authService;
-    private static final String URI = "http://frontend.mo-gether.site/login/oauth/redirect";
+    private static final String URI = "dfrv032cq0wgz.cloudfront.net/login/social";
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
