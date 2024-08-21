144cc420b91ca40042ef19a8073a50f2ad9c5899
sungmingt <qotjdals8313@naver.com>
2024-08-21 13:56:59 +0900

    refactor: remove unused code

--- backend/mogether/src/main/java/mogether/mogether/web/auth/util/PathMatcher.java
+++ backend/mogether/src/main/java/mogether/mogether/web/auth/util/PathMatcher.java
@@ -1,6 +1,7 @@
 package mogether.mogether.web.auth.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 public class PathMatcher {
 
     private PathMatcher(){}
@@ -55,7 +57,6 @@ public class PathMatcher {
         }
 
         for (RequestMatcher matcher : forAnonymousURIMatcher) {
-            System.out.println(matcher.toString());
             if (matcher.matches(request)) {
                 return true;
             }
