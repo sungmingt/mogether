ef3e8546a0736ecd9cc6a05f6866215b0b8a6eb6
sungmingt <qotjdals8313@naver.com>
2024-08-26 12:37:29 +0900

    fix: fix CORS error

--- backend/mogether/src/main/java/mogether/mogether/config/WebConfig.java
+++ backend/mogether/src/main/java/mogether/mogether/config/WebConfig.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RequiredArgsConstructor
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${cors.allowed-origins}")
-    private String[] allowedOrigins;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("userId", "accessToken", "refreshToken")
-                .exposedHeaders("userId", "accessToken", "refreshToken")
-                .maxAge(3600);
-    }
-}
+//@RequiredArgsConstructor
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Value("${cors.allowed-origins}")
+//    private String[] allowedOrigins;
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowCredentials(true)
+//                .allowedOrigins(allowedOrigins)
+//                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("userId", "accessToken", "refreshToken")
+//                .exposedHeaders("userId", "accessToken", "refreshToken")
+//                .maxAge(3600);
+//    }
+//}
