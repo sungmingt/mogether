69fe223ac9bb780fedf08a54ef60593c3291dca6
sungmingt <qotjdals8313@naver.com>
2024-09-21 14:23:55 +0900

    feat: add query method

--- backend/mogether/src/main/java/mogether/mogether/domain/bungae/BungaeUserRepository.java
+++ backend/mogether/src/main/java/mogether/mogether/domain/bungae/BungaeUserRepository.java
@@ -2,9 +2,12 @@ package mogether.mogether.domain.bungae;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BungaeUserRepository extends JpaRepository<BungaeUser, Long> {
 
     Optional<BungaeUser> findByBungaeIdAndUserId(Long bungaeId, Long userId);
+
+    List<BungaeUser> findByUserId(Long userId);
 }
